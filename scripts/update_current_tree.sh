e7cf528681ca5e0b26185b7950e695b6bf1ca568
davidrichardson <david.m.richardson@gmail.com>
2016-03-30 11:55:59 +0100

    correct logging dir

--- scripts/update_current_tree.sh
+++ scripts/update_current_tree.sh
@@ -4,4 +4,4 @@ perl $RESEQTRACK/scripts/file/run_tree_for_ftp.pl \
   -skip_load -skip_archive \
   -dir_to_tree /nfs/faang/vol1/ftp \
   -staging_dir /hps/cstor01/nobackup/faang/archive-staging/ftp \
-  -log_dir /nfs/1000g-work/G1K/work/rseqpipe/faang_ftp_tree_pgtrace_logs
+  -log_dir /homes/farmpipe/logs/faang_ftp_tree_logs
