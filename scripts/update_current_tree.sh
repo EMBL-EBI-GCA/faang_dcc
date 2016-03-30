dcae170c34143a9b231f5bd80d3726287ed7b0df
davidrichardson <david.m.richardson@gmail.com>
2016-03-30 12:50:22 +0100

    do not skip load or archive

--- scripts/update_current_tree.sh
+++ scripts/update_current_tree.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 perl $RESEQTRACK/scripts/file/run_tree_for_ftp.pl \
   $RESEQTRACK_DB_ARGS \
-  -skip_load -skip_archive \
   -dir_to_tree /nfs/faang/vol1/ftp \
   -staging_dir /hps/cstor01/nobackup/faang/archive-staging/ftp \
   -log_dir /homes/farmpipe/logs/faang_ftp_tree_logs
