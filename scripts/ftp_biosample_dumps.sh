b853c42b58e1b803f75544684bb4fd8e2e0ecbfb
Peter Harrison <peter@ebi.ac.uk>
2016-09-02 10:48:16 +0100

    Send summary file to FAANG FTP

--- scripts/ftp_biosample_dumps.sh
+++ scripts/ftp_biosample_dumps.sh
@@ -4,8 +4,8 @@ diff -q /hps/cstor01/nobackup/faang/farmpipe/supporting-info/faang_samples_in_bi
 if ! [[ $? == "0" ]]
 then
   cp -p /hps/cstor01/nobackup/faang/farmpipe/supporting-info/faang_samples_in_biosamples.tsv /hps/cstor01/nobackup/faang/archive-staging/ftp/biosamples/faang_samples_in_biosamples.tsv \
-  && perl /nfs/production/reseq-info/work/farmpipe/reseqtrack/scripts/file/load_files.pl -dbhost mysql-rs-faang-prod -dbport 4478 -dbuser g1krw -dbpass thousandgenomes -dbname faang_archive_staging_track -run -update -do_md5 -file /hps/cstor01/nobackup/faang/archive-staging/ftp/biosamples/faang_samples_in_biosamples.tsv \
-  && perl /nfs/production/reseq-info/work/farmpipe/reseqtrack/scripts/file/archive_files.pl -dbhost mysql-rs-faang-prod -dbport 4478 -dbuser g1krw -dbpass thousandgenomes -dbname faang_archive_staging_track -action  archive -skip -run -priority 99 -file /hps/cstor01/nobackup/faang/archive-staging/ftp/biosamples/faang_samples_in_biosamples.tsv
+  && perl /nfs/production/reseq-info/work/farmpipe/reseqtrack/scripts/file/load_files.pl -dbhost $RESEQTRACK_DB_HOST -dbport $RESEQTRACK_DB_PORT -dbuser $RESEQTRACK_DB_USER -dbpass $RESEQTRACK_DB_PASS -dbname $RESEQTRACK_DB_NAME -run -update -do_md5 -file /hps/cstor01/nobackup/faang/archive-staging/ftp/biosamples/faang_samples_in_biosamples.tsv \
+  && perl /nfs/production/reseq-info/work/farmpipe/reseqtrack/scripts/file/archive_files.pl -dbhost $RESEQTRACK_DB_HOST -dbport $RESEQTRACK_DB_PORT -dbuser $RESEQTRACK_DB_USER -dbpass $RESEQTRACK_DB_PASS -dbname $RESEQTRACK_DB_NAME -action  archive -skip -run -priority 99 -file /hps/cstor01/nobackup/faang/archive-staging/ftp/biosamples/faang_samples_in_biosamples.tsv
 else
   :
 fi;
@@ -13,9 +13,18 @@ diff -q /hps/cstor01/nobackup/faang/farmpipe/supporting-info/rst_samples.tsv /nf
 if ! [[ $? == "0" ]]
 then
   cp -p /hps/cstor01/nobackup/faang/farmpipe/supporting-info/rst_samples.tsv /hps/cstor01/nobackup/faang/archive-staging/ftp/biosamples/rst_samples.tsv \
-  && perl /nfs/production/reseq-info/work/farmpipe/reseqtrack/scripts/file/load_files.pl -dbhost mysql-rs-faang-prod -dbport 4478 -dbuser g1krw -dbpass thousandgenomes -dbname faang_archive_staging_track -run -update -do_md5 -file /hps/cstor01/nobackup/faang/archive-staging/ftp/biosamples/rst_samples.tsv \
-  && perl /nfs/production/reseq-info/work/farmpipe/reseqtrack/scripts/file/archive_files.pl -dbhost mysql-rs-faang-prod -dbport 4478 -dbuser g1krw -dbpass thousandgenomes -dbname faang_archive_staging_track -action  archive -skip -run -priority 99 -file /hps/cstor01/nobackup/faang/archive-staging/ftp/biosamples/rst_samples.tsv
+  && perl /nfs/production/reseq-info/work/farmpipe/reseqtrack/scripts/file/load_files.pl -dbhost $RESEQTRACK_DB_HOST -dbport $RESEQTRACK_DB_PORT -dbuser $RESEQTRACK_DB_USER -dbpass $RESEQTRACK_DB_PASS -dbname $RESEQTRACK_DB_NAME -run -update -do_md5 -file /hps/cstor01/nobackup/faang/archive-staging/ftp/biosamples/rst_samples.tsv \
+  && perl /nfs/production/reseq-info/work/farmpipe/reseqtrack/scripts/file/archive_files.pl -dbhost $RESEQTRACK_DB_HOST -dbport $RESEQTRACK_DB_PORT -dbuser $RESEQTRACK_DB_USER -dbpass $RESEQTRACK_DB_PASS -dbname $RESEQTRACK_DB_NAME -action  archive -skip -run -priority 99 -file /hps/cstor01/nobackup/faang/archive-staging/ftp/biosamples/rst_samples.tsv
 else
   :
 fi;
-perl /nfs/production/reseq-info/work/farmpipe/reseqtrack/scripts/file/cleanup_archive.pl -dbhost mysql-rs-faang-prod -dbport 4478 -dbuser g1krw -dbpass thousandgenomes -dbname faang_archive_staging_track
+diff -q /hps/cstor01/nobackup/faang/farmpipe/supporting-info/biosample_summary.tsv /nfs/faang/vol1/ftp/biosamples/biosample_summary.tsv 1>/dev/null
+if ! [[ $? == "0" ]]
+then
+  cp -p /hps/cstor01/nobackup/faang/farmpipe/supporting-info/biosample_summary.tsv /hps/cstor01/nobackup/faang/archive-staging/ftp/biosamples/biosample_summary.tsv \
+  && perl /nfs/production/reseq-info/work/farmpipe/reseqtrack/scripts/file/load_files.pl -dbhost $RESEQTRACK_DB_HOST -dbport $RESEQTRACK_DB_PORT -dbuser $RESEQTRACK_DB_USER -dbpass $RESEQTRACK_DB_PASS -dbname $RESEQTRACK_DB_NAME -run -update -do_md5 -file /hps/cstor01/nobackup/faang/archive-staging/ftp/biosamples/biosample_summary.tsv \
+  && perl /nfs/production/reseq-info/work/farmpipe/reseqtrack/scripts/file/archive_files.pl -dbhost $RESEQTRACK_DB_HOST -dbport $RESEQTRACK_DB_PORT -dbuser $RESEQTRACK_DB_USER -dbpass $RESEQTRACK_DB_PASS -dbname $RESEQTRACK_DB_NAME -action  archive -skip -run -priority 99 -file /hps/cstor01/nobackup/faang/archive-staging/ftp/biosamples/biosample_summary.tsv
+else
+  :
+fi;
+perl /nfs/production/reseq-info/work/farmpipe/reseqtrack/scripts/file/cleanup_archive.pl -dbhost $RESEQTRACK_DB_HOST -dbport $RESEQTRACK_DB_PORT -dbuser $RESEQTRACK_DB_USER -dbpass $RESEQTRACK_DB_PASS -dbname $RESEQTRACK_DB_NAME
