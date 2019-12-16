1d2aa2b322b569699636f0885234b153a242f472
peterwharrison <peter@ebi.ac.uk>
2019-12-16 16:52:26 +0000

    Deprecate chip input control

--- scripts/chip_input_control.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-CONFIG_DIR=$SCRIPT_DIR/../config
-OUTPUT_DIR=$SUPPORTING_INFO
-
-perl $SCRIPT_DIR/chip_input_control.pl $RESEQTRACK_DB_ARGS -output_file $OUTPUT_DIR/input_lookup_auto.json
-
---
+++ scripts/cleanup_archive.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+perl $RESEQTRACK/scripts/file/cleanup_archive.pl $RESEQTRACK_DB_ARGS -sleep 1
